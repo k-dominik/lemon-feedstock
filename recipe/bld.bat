4e0afc53283f945f67a35de75b38c9ec09c733f1
k-dominik <k-dominik@users.noreply.github.com>
2020-09-16 13:35:23 +0200

    explicit config type, cmake export symbols

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,34 @@
 mkdir build
 cd build
 
+set CONFIGURATION="Release"
+
 cmake ^
     -G "%CMAKE_GENERATOR%" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_POSITION_INDEPENDENT_CODE=1 ^
     -DBUILD_SHARED_LIBS=1 ^
+    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=1 ^
+    -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
     ..
 
-cmake --build . --config Release
+if errorlevel 1 exit 1
 
-cmake --build . --config Release --target error_test
-cmake --build . --config Release --target bfs_test
+cmake --build . --config %CONFIGURATION%
+
+if errorlevel 1 exit 1
+
+cmake --build . --config %CONFIGURATION% --target error_test
+if errorlevel 1 exit 1
+cmake --build . --config %CONFIGURATION% --target bfs_test
+if errorlevel 1 exit 1
+
 
 ctest -R error_test
 ctest -R bfs_test
 
-cmake --build . --config Release --target install
+cmake --build . --config %CONFIGURATION% --target install
+
+if errorlevel 1 exit 1
+
