01e7e44d4e008e7050a98bf6e03cc25fefad3662
k-dominik <k-dominik@users.noreply.github.com>
2020-09-16 13:20:24 +0200

    add patch for windows dll exports

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,12 +9,22 @@ cmake ^
     -DBUILD_SHARED_LIBS=1 ^
     ..
 
+if errorlevel 1 exit 1
+
 cmake --build . --config Release
 
+if errorlevel 1 exit 1
+
 cmake --build . --config Release --target error_test
+if errorlevel 1 exit 1
 cmake --build . --config Release --target bfs_test
+if errorlevel 1 exit 1
 
 ctest -R error_test
+if errorlevel 1 exit 1
 ctest -R bfs_test
+if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
+
+if errorlevel 1 exit 1
